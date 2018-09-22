a34de060edea7a688a36dbb5a6f83fdf3cbbaceb
Wasif Khan <waasif.khaan@gmail.com>
2018-09-22 22:28:32 +0000

    automatically backed up by learn

--- spec/helpers/students_helper_spec.rb
+++ spec/helpers/students_helper_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 # Specs in this file have access to a helper object that includes
 # the StudentsHelper. For example:
 #
+<<<<<<< HEAD
 # describe StudentsHelper do
 #   describe "string concat" do
 #     it "concats two strings with spaces" do
@@ -10,6 +11,15 @@ require 'rails_helper'
 #     end
 #   end
 # end
+=======
+describe StudentsHelper do
+  describe "string concat" do
+    it "concats two strings with spaces" do
+      expect(helper.concat_strings("this","that")).to eq("this that")
+    end
+  end
+end
+>>>>>>> 7c3792d2400a983fba3fe78d9d4b552abdfe47e0
 RSpec.describe StudentsHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
